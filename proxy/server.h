0f7d2639f7a06ce2bfbaa0d266aea6a72c44996d
Altanb21 <94198465+Altanb21@users.noreply.github.com>
2022-07-25 15:08:52 +0300

    Update server.h

--- proxy/server.h
+++ proxy/server.h
@@ -5,11 +5,11 @@
 #include "world.h"
 
 class server {
+
    private:
     ENetHost* m_proxy_server;
     ENetHost* m_real_server;
     ENetPeer* m_server_peer;
-    ENetPeer* m_gt_peer;
   
     
     void handle_outgoing();
@@ -17,19 +17,27 @@ class server {
     bool connect();
     void disconnect(bool reset);
    public:
-          struct Item {
+       ENetPeer* m_gt_peer;
+
+       struct Item {
            uint16_t id;
            uint8_t count;
            uint8_t type;
        };
        static std::vector<server::Item> inventory;
+
+       void disconnectsr(bool reset);
+
     int m_user = 0;
     int m_token = 0;
     std::string m_server = "213.179.209.168";
-    std::string meta = "NULL";
+    int m_port = 17250;
+    std::string serverz = m_server;
+    int portz = m_port;
+    int m_proxyport = 17250;
+    std::string ipserver = "127.0.0.1";
+    std::string create = "0.0.0.0";
 
-    int m_port = 17198;
-    int m_proxyport = 17191;
     world m_world;
     bool start();
     void quit();
@@ -38,6 +46,9 @@ class server {
     void send(bool client, int32_t type, uint8_t* data, int32_t len);
     void send(bool client, variantlist_t& list, int32_t netid = -1, int32_t delay = 0);
     void send(bool client, std::string packet, int32_t type = 2);
+    void SendPacketRaw(bool client, int a1, void* packetData, size_t packetDataSize, void* a4, int packetFlag);
+    void addBlock(int a, int b, int c);
+    void sendState(int netid);
     void poll();
 };
 extern server* g_server;
